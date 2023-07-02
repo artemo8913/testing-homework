bcfc3553ee1d07d1f4d179f36be40d4c121161f5
Spiryaev <kaline-son@ya.ru>
2023-07-02 18:26:05 +0700

    .

--- test/unit/Application.test.tsx
+++ test/unit/Application.test.tsx
@@ -3,13 +3,14 @@ import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { Provider } from "react-redux";
 
+import events from "@testing-library/user-event";
 import { initStore } from "../../src/client/store";
 import { Application } from "../../src/client/Application";
 import { CartApi, ExampleApi } from "../../src/client/api";
 import React from "react";
 
 describe("Проверка работы приложения", () => {
-  it("в наличии бургер меню", () => {
+  it("ссылки в шапке в наличии", async () => {
     const basename = "/hw/store";
     const api = new ExampleApi(basename);
     const cart = new CartApi();
@@ -22,8 +23,15 @@ describe("Проверка работы приложения", () => {
       </MemoryRouter>
     );
 
-    const { getByTestId, container } = render(application);
-    console.log(container.outerHTML);
-    console.log(getByTestId("menu").outerHTML);
+    const { container } = render(application);
+
+    const catalogLink = container.querySelector("[href $='/catalog']");
+    const deliveryLink = container.querySelector("[href $='/delivery']");
+    const contactsLink = container.querySelector("[href $='/contacts']");
+    const cartLink = container.querySelector("[href $='/cart']");
+    expect(catalogLink).not.toBe(null);
+    expect(deliveryLink).not.toBe(null);
+    expect(contactsLink).not.toBe(null);
+    expect(cartLink).not.toBe(null);
   });
 });
